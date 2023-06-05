f8375348ee0b471a519543599c48c71844411805
Shreya <shreya.chkm@gmail.com>
2023-06-05 16:14:39 +0600

    few update

--- src/main.js
+++ src/main.js
@@ -1,89 +1,96 @@
 import './style.css'
-
-// import { setupCounter } from './counter.js'
-
 import { makeCanvas } from './popup';
 
-export function labelbakerBanner(config){
-
-  const makeId = (length) => {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    let counter = 0;
-    while (counter < length) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      counter += 1;
-    }
-    return result;
+const makeId = (length) => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for(let counter = 0; counter < length; counter++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
- const stageID = makeId(4);
- const canvasID = makeId(4);
-  const bannerHTML = `
-  <div id="${stageID}">
-    <div id="${canvasID}">
-    </div>
-  </div>
-  `
-  document.querySelector('#' + config.container).insertAdjacentHTML('beforeend', bannerHTML)
+
+const makeHTML = (stageID, canvasID, isPopup=false) => {
+  return isPopup 
+  ? `<div>
+      <div class="labelbaker-popup-container">
+      <div id="labelbaker-popup" class="labelbaker-popup-content">
+      <button class="labelbaker-close-button">&times;</button>
+      <div id="${stageID}">
+      <div id="${canvasID}">
+      </div>
+      </div>
+      </div>
+      </div>
+    </div>` 
+  : `<div id="${stageID}">
+      <div id="${canvasID}">
+      </div>
+    </div>`
+}
+
+const fetchTemplateData = async (url, canvasID, stageID) => {
+  try {
+    let response = await fetch(url);
+    let templateData = await response.json();
+    await makeCanvas(canvasID, stageID, templateData);
+    console.log('Template data loaded successfully');
+  } catch (err) {
+    console.error('Failed to load template data: ', err);
+  }
+}
+
+export function labelbakerBanner(config) {
+  const stageID = makeId(4);
+  const canvasID = makeId(4);
+  const bannerHTML = makeHTML(stageID, canvasID);
+
+  document.getElementById(config.container).insertAdjacentHTML('beforeend', bannerHTML)
   document.getElementById(config.container).style.maxWidth = config.width;
-  let url = config.url
-  fetch(url).then(res => res.json()).then(templateData => {
 
-    makeCanvas(canvasID, stageID, templateData).then(e => {
-      
-
-      console.log('loaded')
-
-    })
-  }).catch(err => console.log(err));
+  fetchTemplateData(config.url, canvasID, stageID);
 }
 
 export function labelbakerPopup(config) {
-
-  const popupHTML = `
-<div>
-  <button id="show">show</button>
-  <div class="labelbaker-popup-container">
-  <div id="labelbaker-popup" class="labelbaker-popup-content">
-  <button class="labelbaker-close-button">&times;</button>
-  <div id="stageID">
-  <div id="canvasID">
-  </div>
-  </div>
-  </div>
-  </div>
-</div>`;
+  const stageID = makeId(4);
+  const canvasID = makeId(4);
+  const popupHTML = makeHTML(stageID, canvasID, true);
 
   document.querySelector('body').insertAdjacentHTML('beforeend', popupHTML)
 
   const closeButton = document.querySelector('.labelbaker-close-button');
   const popupContainer = document.querySelector('.labelbaker-popup-container');
+  const popupCanvas = document.getElementById(canvasID);
 
-  let lp = sessionStorage.getItem('labelbaker-popup');
-  if (lp) {
-    popupContainer.style.display = 'none';
-    return;
-  } else {
-    sessionStorage.setItem('labelbaker-popup', true)
+  popupCanvas.style.borderRadius = '10px';
+  popupCanvas.style.boxShadow = '0 2px 8px 0 rgba(63, 69, 81, 0.16)';
+
+  var style = document.createElement('style');
+  style.innerHTML = `
+  #${canvasID} > .konvajs-content>canvas {
+    border-radius: 10px;
+    box-shadow: 0 2px 8px 0 rgba(63, 69, 81, 0.16);
+  }`;
+  document.head.appendChild(style);
+
+  if (config.showOnce) {
+    let lp = sessionStorage.getItem('labelbaker-popup');
+    if (lp) {
+      popupContainer.style.display = 'none';
+      return;
+    } else {
+      sessionStorage.setItem('labelbaker-popup', true)
+    }
   }
 
   document.getElementById('labelbaker-popup').style.maxWidth = config.width;
-  let url = config.url
-  fetch(url).then(res => res.json()).then(templateData => {
 
-    makeCanvas('canvasID', 'stageID', templateData).then(e => {
-      popupContainer.style.display = 'flex';
-      popupContainer.style.visibility = 'visible'
-      closeButton.style.display = 'block';
-
-      console.log('loaded')
-
-    })
-  }).catch(err => console.log(err));
-
-
-
+  fetchTemplateData(config.url, canvasID, stageID)
+  .then(() => {
+    popupContainer.style.display = 'flex';
+    closeButton.style.display = 'block';
+  });
 
   closeButton.addEventListener('click', () => {
     popupContainer.style.display = 'none';
@@ -93,28 +100,24 @@ export function labelbakerPopup(config) {
     if (e.target.className === 'labelbaker-popup-container') {
       popupContainer.style.display = 'none';
     }
-  })
-
-  document.getElementById('show').addEventListener('click', function () {
-    popupContainer.style.display = 'flex';
-  })
-
+  });
 }
 
-const conf = {
-  url: "https://sample-file.com/i/53616c7465645f5f275a3d6ba7fdf6c1805a9be85219b78991c5dfa33b169f731572abbe72be885362db23c7ef73fe99/53616c7465645f5f4a18012b7dbc8a71c06b55eb518a71d33770db255d112afe",
-  width: "500px",
-};
+// const conf = {
+//   url: "https://sample-file.com/i/53616c7465645f5f275a3d6ba7fdf6c1805a9be85219b78991c5dfa33b169f731572abbe72be885362db23c7ef73fe99/53616c7465645f5f4a18012b7dbc8a71c06b55eb518a71d33770db255d112afe",
+//   width: "500px",
+  
+// };
 
-const bannerConfig = {
-  url: "https://sample-file.com/i/53616c7465645f5f275a3d6ba7fdf6c1805a9be85219b78991c5dfa33b169f731572abbe72be885362db23c7ef73fe99/53616c7465645f5f4a18012b7dbc8a71c06b55eb518a71d33770db255d112afe",
-  width: "500px",
-  container:'banner'
-}
+// const bannerConfig = {
+//   url: "https://sample-file.com/i/53616c7465645f5f275a3d6ba7fdf6c1805a9be85219b78991c5dfa33b169f731572abbe72be885362db23c7ef73fe99/53616c7465645f5f4a18012b7dbc8a71c06b55eb518a71d33770db255d112afe",
+//   width: "500px",
+//   container: 'banner'
+// }
 
-labelbakerBanner(bannerConfig)
+// labelbakerBanner(bannerConfig)
 
-labelbakerPopup(conf);
+// labelbakerPopup(conf);
 
 
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-export default {
+import { defineConfig } from 'vite'
+import commonjs from '@rollup/plugin-commonjs'
+export default defineConfig({
+  plugins: [commonjs()],
     build: {
       lib: {
         entry: 'src/main.js',
-        name: 'labelbaker'
+        name: 'labelbaker',
+        fileName: (format) => `labelbaker.js`
       },
       rollupOptions: {
         output: {
@@ -12,5 +16,5 @@ export default {
         }
       }
     }
-  }
+  })
   
