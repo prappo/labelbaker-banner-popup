fa84862cfdeaa08127d0131ed69cde4c0fd41323
Shreya <shreya.chkm@gmail.com>
2023-06-05 18:25:15 +0600

    few update

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,18 @@ import { defineConfig } from 'vite'
 import commonjs from '@rollup/plugin-commonjs'
 export default defineConfig({
   plugins: [commonjs()],
-    build: {
-      lib: {
-        entry: 'src/main.js',
+  build: {
+    lib: {
+      entry: 'src/main.js',
+      name: 'labelbaker',
+      fileName: (format) => `labelbaker.js`
+    },
+    rollupOptions: {
+      output: {
+        format: 'umd',
         name: 'labelbaker',
-        fileName: (format) => `labelbaker.js`
-      },
-      rollupOptions: {
-        output: {
-          format: 'umd',
-          name: 'labelbaker',
-          extend: true
-        }
+        extend: true
       }
     }
-  })
-  
+  }
+})
